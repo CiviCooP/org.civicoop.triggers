86133a15cf869e9b7e51e91b52c259390b53ed71
Jaap Jansma <info@edeveloper.nl>
2014-04-07 16:30:47 +0200

    added bao

---
+++ CRM/Triggers/BAO/TriggerAction.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * This is the BAO file for the trigger action table
+ */
+
+class CRM_Triggers_BAO_TriggerAction extends CRM_Triggers_DAO_TriggerAction {
+
+  /**
+   * Find trigger actions which are ready for processing
+   * 
+   */
+  public static function findForProcessing() {
+    
+  }
+  
+}
+
