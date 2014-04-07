38759ea16c811b549d458ef4c7518357447ed27b
Jaap Jansma <info@edeveloper.nl>
2014-04-07 16:45:51 +0200

    added processing

--- CRM/Triggers/BAO/TriggerAction.php
+++ CRM/Triggers/BAO/TriggerAction.php
@@ -9,9 +9,21 @@ class CRM_Triggers_BAO_TriggerAction extends CRM_Triggers_DAO_TriggerAction {
   /**
    * Find trigger actions which are ready for processing
    * 
+   * @param bool $fetchFirst fetch the first record
    */
-  public static function findForProcessing() {
+  public static function findForProcessing($fetchFirst = FALSE) {
+    $trigger_action = new CRM_Triggers_DAO_TriggerAction();
     
+    $trigger_action->selectAdd();
+    $trigger_action->selectAdd("*");
+    $trigger_action->whereAdd('is_active = 1');
+    $trigger_action->whereAdd('(start_date IS NULL OR start_date <= CURDATE())');
+    $trigger_action->whereAdd('(end_date IS NULL OR end_date >= CURDATE())');
+    $trigger_action->whereAdd('(next_run <= CURDATE())');
+    
+    $trigger_action->find($fetchFirst);
+    
+    return $trigger_action;
   }
   
 }
--- api/v3/TriggerAction/Process.php
+++ api/v3/TriggerAction/Process.php
@@ -9,7 +9,7 @@
  * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
  */
 function _civicrm_api3_trigger_action_process_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  //no parameters for this cron job
 }
 
 /**
@@ -22,19 +22,14 @@ function _civicrm_api3_trigger_action_process_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_trigger_action_process($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array( // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  } else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
+  $actions = CRM_Triggers_BAO_TriggerAction::findForProcessing(FALSE);
+  while ($actions->fetch()) {
+    //process this trigger action
+    //here comes all the logic etc....
+    
+    //this consists of the following steps
+    //1. retrieve the entities which match the condition of the trigger
+    //2. prepare the api action with the entity
   }
 }
 
