8b090b7065d7ff2f0db238a58adafdc1027f50aa
Jaap Jansma <info@edeveloper.nl>
2014-04-08 15:22:37 +0200

    parsing of parameters

--- CRM/Triggers/BAO/ActionRule.php
+++ CRM/Triggers/BAO/ActionRule.php
@@ -9,10 +9,10 @@ class CRM_Triggers_BAO_ActionRule extends CRM_Triggers_DAO_ActionRule {
    * Process the action for the given entity ($objRef)
    * @param CRM_Core_DAO $objRef
    */
-  public function processEntity(CRM_Core_DAO $objRef) {
-    $params = $this->parseParms($objRef);
+  public function processEntity(CRM_Core_DAO $objRef, CRM_Triggers_BAO_TriggerRule $trigger_rule) {
+    $params = $this->parseParms($objRef, $trigger_rule);
     
-    civicrm_api3($this->entity, $this->action, $params);
+    //civicrm_api3($this->entity, $this->action, $params);
   }
   
   /**
@@ -21,8 +21,57 @@ class CRM_Triggers_BAO_ActionRule extends CRM_Triggers_DAO_ActionRule {
    * @param CRM_Core_DAO $objRef
    * @return array
    */
-  protected function parseParams(CRM_Core_DAO $objRef) {
-    return array();
+  protected function parseParams(CRM_Core_DAO $objRef, CRM_Triggers_BAO_TriggerRule $trigger_rule) {
+    $return = array();
+    
+    $params = explode("&", $this->params);
+    $fields = $objRef->fields();
+    
+    $hooks = CRM_Utils_Hook::singleton();
+    $hooks->invoke(5,
+      $params, $return, $objref, $trigger_rule, $this,
+      'civicrm_trigger_action_parse_params'
+      );
+    
+    //loop trhough all parameters and replace their value if needed
+    foreach($params as $key => $val) {  
+      //check if the val is encapsulated into brackets { }      
+      //e.g. {contribution.status_id}      
+         
+      $matches = array();      
+      if (!isset($return[$key]) && preg_match("/{(.*)\.(.*)}/", $val, $matches)) {
+        //value looks like {entity.field}
+        //so we split this and we check if objRef is of entty and the field exist on objectref
+        $entityName = CRM_Triggers_Utils::camelCaseEntity($matches[1]);
+        $fieldName = $matches[2];
+        
+        $entityType = CRM_Core_DAO_AllCoreTables::getFullName($entityName);
+        if ($entityType == NULL) {
+          throw new CRM_Triggers_Exception_DAO_Not_Found("Entity ".$entityName." has no DAO");
+        }
+        //check if objRef is an instanceof $entityType
+        if ($objRef instanceof $entityType) {      
+          if (isset($fields[$fieldName])) {
+            $return[$key] = $objRef->$fieldName;
+          }
+        }
+      } 
+      
+      $matches = array();
+      if (!isset($return[$key]) && preg_match("/{(.*)}/", $val, $matches)) {
+        //value looks like {field}
+        //check if field exist on objRef.
+        $fieldName = $matches[1];
+        if (isset($fields[$fieldName])) {
+          $return[$key] = $objRef->$fieldName;
+        }
+      }
+      
+      if (!isset($return[$key])) {
+        $return[$key] = $val;
+      }
+    }
+    return $return;
   }
   
   public static function findByActionId($action_id) {
