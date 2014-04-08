07b0136a2adb98ba1a92e224376c67a1ff432048
Jaap Jansma <info@edeveloper.nl>
2014-04-08 09:47:20 +0200

    find entities for processing

--- CRM/Triggers/BAO/TriggerAction.php
+++ CRM/Triggers/BAO/TriggerAction.php
@@ -12,7 +12,7 @@ class CRM_Triggers_BAO_TriggerAction extends CRM_Triggers_DAO_TriggerAction {
    * @param bool $fetchFirst fetch the first record
    */
   public static function findForProcessing($fetchFirst = FALSE) {
-    $trigger_action = new CRM_Triggers_DAO_TriggerAction();
+    $trigger_action = new CRM_Triggers_BAO_TriggerAction();
     
     $trigger_action->selectAdd();
     $trigger_action->selectAdd("*");
@@ -24,6 +24,31 @@ class CRM_Triggers_BAO_TriggerAction extends CRM_Triggers_DAO_TriggerAction {
     $trigger_action->find($fetchFirst);
     
     return $trigger_action;
+  }
+  
+  /**
+   * Returns the found entities which should be processed by the trigger
+   */
+  public function findEntities() {
+    $this->validate();
+    
+    $trigger = new CRM_Core_BAO_TriggerRule();
+    $trigger->selectAdd();
+    $trigger->selectAdd('*');
+    $trigger->whereAdd("id = '".$this->trigger_rule_id."'");
+    $trigger->find(TRUE);
+    
+    //now build condition query
+  }
+  
+  /**
+   * Checks if current object is valid to use
+   * Throws an error if not
+   */
+  protected function validate() {
+    if (empty($this->trigger_rule_id)) {
+      throw new CRM_Triggers_Exception_InvalidTriggerActionEntity("Trigger rule ID is not set");
+    }
   }
   
 }
--- CRM/Triggers/BAO/TriggerRule.php
+++ CRM/Triggers/BAO/TriggerRule.php
@@ -6,12 +6,5 @@
 
 class CRM_Triggers_BAO_TriggerRule extends CRM_Triggers_DAO_TriggerRule {
   
-  /**
-   * Returns the found entities which should be processed by the trigger
-   */
-  public static function findEntities() {
-    
-  }
-  
 }
 
