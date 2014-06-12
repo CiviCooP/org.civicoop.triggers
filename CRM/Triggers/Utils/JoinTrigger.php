c597d0c78947ed48a51695f0a1f4a001ef51074c
Jaap Jansma <info@edeveloper.nl>
2014-06-12 13:29:47 +0200

    fxied error with joining entities asssuming the contact_id as link

--- CRM/Triggers/Utils/JoinTrigger.php
+++ CRM/Triggers/Utils/JoinTrigger.php
@@ -46,6 +46,15 @@ class CRM_Triggers_Utils_JoinTrigger {
         }
       }
     }
+
+		//create a join on contact_id if both classes have a link to contact
+		if (CRM_Triggers_Utils_JoinTrigger::hasDaoALinkToContact($trigger_dao_class)) {
+			foreach($dao_classes as $dao_class) {
+				if (CRM_Triggers_Utils_JoinTrigger::hasDaoALinkToContact($dao_class)) {
+						return new CRM_Triggers_QueryBuilder_Condition("`".$trigger_dao_class::getTableName()."`.`contact_id` = `".$dao_class::getTableName()."`.`contact_id`");
+				}
+			}
+		}
     
     return false;
   }
@@ -64,19 +73,32 @@ class CRM_Triggers_Utils_JoinTrigger {
       //not in every version of civicrm the reference columns are defined.
       //so we have to add them manually.
       //first check if a contact_id field exist. If so add a ref to the contact object
-      $fields = $dao_class::fields();
-      $keyFields = array();
-      if (method_exists($dao_class, 'fieldKeys')) {
-        $keyFields = $dao_class::fieldKeys();
-      }
-      if (isset($fields['contribution_contact_id'])) {
-        $references = array(new CRM_Triggers_Utils_EntityReference($dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
-      } elseif (isset($fields['contact_id']) || isset($keyFields['contact_id'])) {
+			if (CRM_Triggers_Utils_JoinTrigger::hasDaoALinkToContact($dao_class)) {
         $references = array(new CRM_Triggers_Utils_EntityReference($dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
       }
     }
     return $references;
   }
+
+	protected static function hasDaoALinkToContact($dao_class) {
+		if ($dao_class::getTableName() == 'civicrm_contact') {
+			return false;
+		}
+		
+		//not in every version of civicrm the reference columns are defined.
+    //so we have to add them manually.
+    //first check if a contact_id field exist. If so add a ref to the contact object
+    $fields = $dao_class::fields();
+    $keyFields = array();
+    if (method_exists($dao_class, 'fieldKeys')) {
+      $keyFields = $dao_class::fieldKeys();
+    }
+    if (isset($fields['contribution_contact_id'])) {
+      return true;
+    } elseif (isset($fields['contact_id']) || isset($keyFields['contact_id'])) {
+      return true;
+    }
+		return false;
+	}
   
 }
-
