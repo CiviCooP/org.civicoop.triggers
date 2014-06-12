778deca51994b615baa0086b762f103b54349f8e
Jaap Jansma <info@edeveloper.nl>
2014-06-12 12:31:49 +0200

    created seperate functionality to retrieve references for a dao

--- CRM/Triggers/Utils/JoinTrigger.php
+++ CRM/Triggers/Utils/JoinTrigger.php
@@ -23,25 +23,7 @@ class CRM_Triggers_Utils_JoinTrigger {
    * @return boolean|\CRM_Triggers_QueryBuilder_Condition
    */
   public static function createJoinCondition($dao_classes, $trigger_dao_class) {
-    $references = array();
-    if (method_exists($trigger_dao_class, 'getReferenceColumns')) {
-      $references = CRM_Triggers_Utils_EntityReference::convertReferences($trigger_dao_class::getReferenceColumns());
-    } else {
-      //not in every version of civicrm the reference columns are defined.
-      //so we have to add them manually.
-      //first check if a contact_id field exist. If so add a ref to the contact object
-      $fields = $trigger_dao_class::fields();
-      $keyFields = array();
-      if (method_exists($trigger_dao_class, 'fieldKeys')) {
-        $keyFields = $trigger_dao_class::fieldKeys();
-      }
-      if (isset($fields['contribution_contact_id'])) {
-        $references = array(new CRM_Triggers_Utils_EntityReference($trigger_dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
-      } elseif (isset($fields['contact_id']) || isset($keyFields['contact_id'])) {
-        $references = array(new CRM_Triggers_Utils_EntityReference($trigger_dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
-      }
-    }    
-    
+    $references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($trigger_dao_class);    
     foreach($references as $ref) {
       foreach($dao_classes as $dao_class) {
         if ($ref->getTargetTable() == $dao_class::getTableName()) {
@@ -54,7 +36,7 @@ class CRM_Triggers_Utils_JoinTrigger {
     }
     
     foreach($dao_classes as $dao_class) {
-      $references = $dao_class::getReferenceColumns();
+      $references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($dao_class);
       foreach($references as $ref) {
         if ($ref->getTargetTable() == $trigger_dao_class::getTableName()) {
           return new CRM_Triggers_QueryBuilder_Condition(
@@ -68,6 +50,33 @@ class CRM_Triggers_Utils_JoinTrigger {
     return false;
   }
   
+  /**
+   * Get refernces to other objects from a DAO 
+   * 
+   * @param type $dao_class
+   * @return \CRM_Triggers_Utils_EntityReference
+   */
+  protected static function getDaoReferenceColumns($dao_class) {
+    $references = array();
+    if (method_exists($dao_class, 'getReferenceColumns')) {
+      $references = CRM_Triggers_Utils_EntityReference::convertReferences($dao_class::getReferenceColumns());
+    } else {
+      //not in every version of civicrm the reference columns are defined.
+      //so we have to add them manually.
+      //first check if a contact_id field exist. If so add a ref to the contact object
+      $fields = $dao_class::fields();
+      $keyFields = array();
+      if (method_exists($dao_class, 'fieldKeys')) {
+        $keyFields = $dao_class::fieldKeys();
+      }
+      if (isset($fields['contribution_contact_id'])) {
+        $references = array(new CRM_Triggers_Utils_EntityReference($dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
+      } elseif (isset($fields['contact_id']) || isset($keyFields['contact_id'])) {
+        $references = array(new CRM_Triggers_Utils_EntityReference($dao_class::getTableName() , 'contact_id', 'civicrm_contact', 'id'));
+      }
+    }
+    return $references;
+  }
   
 }
 
