1cb721642254e9bda0f1d3c9ce1e0f4d14296dac
Jaap Jansma <info@edeveloper.nl>
2014-10-23 12:07:35 +0200

    update

--- CRM/Triggers/Utils/JoinTrigger.php
+++ CRM/Triggers/Utils/JoinTrigger.php
@@ -23,9 +23,7 @@ class CRM_Triggers_Utils_JoinTrigger {
    * @return boolean|\CRM_Triggers_QueryBuilder_Condition
    */
   public static function createJoinCondition($dao_classes, $trigger_dao_class, $table_alias) {
-    $refs = array();
     $references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($trigger_dao_class);    
-    $refs[$trigger_dao_class] = $references;
     foreach($references as $ref) {
       foreach($dao_classes as $alias => $dao_class) {
         if ($ref->getTargetTable() == $dao_class::getTableName()) {
@@ -39,13 +37,28 @@ class CRM_Triggers_Utils_JoinTrigger {
     
     foreach($dao_classes as $alias => $dao_class) {
       $references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($dao_class);
-      $refs[$dao_class] = $references;
       foreach($references as $ref) {
         if ($ref->getTargetTable() == $trigger_dao_class::getTableName()) {
           return new CRM_Triggers_QueryBuilder_Condition(
             "`".$table_alias."`.`".$ref->getTargetKey() . 
             "` = `".$alias."`.`".$ref->getReferenceKey()."`"
           );
+        }
+      }
+    }
+    
+    //check for linking entity
+    foreach($dao_classes as $alias => $dao_class) {
+      $references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($dao_class);
+      $trigger_references = CRM_Triggers_Utils_JoinTrigger::getDaoReferenceColumns($trigger_dao_class);
+      foreach($trigger_references as $trigger_ref) {
+        foreach($references as $ref) {
+          if ($ref->getTargetTable() == $trigger_ref->getTargetTable() && $ref->getTargetKey() == $trigger_ref->getTargetKey()) {
+            return new CRM_Triggers_QueryBuilder_Condition(
+              "`".$table_alias."`.`".$ref->getReferenceKey() . 
+              "` = `".$alias."`.`".$ref->getReferenceKey()."`"
+            );
+          }
         }
       }
     }
@@ -58,7 +71,7 @@ class CRM_Triggers_Utils_JoinTrigger {
 				}
 			}
 		}
-    var_dump($refs); exit();
+
     return false;
   }
   
